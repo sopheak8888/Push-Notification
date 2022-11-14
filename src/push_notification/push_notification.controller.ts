bc11cc3b4d6d47623f2b47ea6161850f0798bb4d
Lim Sopheak <limsopheak89@gmail.com>
2022-11-14 16:39:44 +0700

    master

--- src/push_notification/push_notification.controller.ts
+++ src/push_notification/push_notification.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { PushNotificationService } from './push_notification.service';
 import { PushNotificationDto } from './dto/push_notification.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('push_notification')
 export class PushNotificationController {
@@ -9,6 +10,7 @@ export class PushNotificationController {
   ) {}
 
   @Post()
+  @UseGuards(AuthGuard())
   async sendPushNotification(@Body() pushNotificationDto: PushNotificationDto) {
     return await this.pushNotificationService.sendPushNotification(
       pushNotificationDto,
--- src/push_notification/push_notification.module.ts
+++ src/push_notification/push_notification.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
 import { PushNotificationController } from './push_notification.controller';
 import { PushNotificationService } from './push_notification.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [PushNotificationController],
   providers: [PushNotificationService],
 })
